cbb0d8ed51dea5450f220bd2440d7b4c3e5fd151
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-09-26 20:33:20 +0530

    Initial layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import "./App.css";
+import logo from "./assets/images/logo.svg";
+import bgTriangle from "./assets/images/bg-triangle.svg";
+import paper from "./assets/images/icon-paper.svg";
+import rock from "./assets/images/icon-rock.svg";
+import scissors from "./assets/images/icon-scissors.svg";
 
 function App() {
-  return <div>Let's play Rock, Paper, Scissors</div>;
+  return (
+    <div>
+      <header className="header">
+        <div className="logo-container">
+          <img src={logo} alt="logo" height={85} />
+        </div>
+        <div className="score">
+          <p className="score-text">Score</p>
+          <p className="score-value">12</p>
+        </div>
+      </header>
+      <main>
+        <div className="game-content">
+          <img className="bg-game" src={bgTriangle} alt="bg-triangle" />
+          <button className="btn-round btn-paper border-gradient">
+            <img className="paper" src={paper} alt="paper-icon" />
+          </button>
+          <button className="btn-round btn-scissors">
+            <img className="scissors" src={scissors} alt="scissors-icon" />
+          </button>
+          <button className="btn-round btn-rock">
+            <img className="rock" src={rock} alt="rock-icon" />
+          </button>
+        </div>
+        <button className="btn-outline">Rules</button>
+      </main>
+    </div>
+  );
 }
 
 export default App;
